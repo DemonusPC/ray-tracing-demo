307c0050d3531ff65fb5a84dfe271ca3c89a63cc
Demonus <pogon.ap@hotmail.com>
2020-05-21 23:12:35 +0100

    13. Final Image

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ use models::Dielectric;
 use models::Lambertian;
 use models::Metal;
 use models::Sphere;
-use utility::random_double;
+use utility::{random_double, random_double_from_values};
 
 fn ray_color(r: &Ray, world: &dyn HitAble, depth: i32) -> Vec3 {
     let mut rec = HitRecord::empty();
@@ -55,51 +55,88 @@ fn ray_color(r: &Ray, world: &dyn HitAble, depth: i32) -> Vec3 {
     (Vec3::new(1.0, 1.0, 1.0) * (1.0 - t)) + (Vec3::new(0.5, 0.7, 1.0) * t)
 }
 
+fn random_scene() -> HitAbleList {
+    let mut world = HitAbleList::new();
+
+    let ground_material = Rc::new(Lambertian::new(Vec3::new(0.5, 0.5, 0.5)));
+    world.add(Rc::new(Sphere::new(
+        Vec3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
+    )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = random_double();
+
+            let center = Vec3::new(
+                (a as f64) + 0.9 * random_double(),
+                0.2,
+                b as f64 + 0.9 * random_double(),
+            );
+
+            if (center - Vec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo = Vec3::random() * Vec3::random();
+                    let sphere_material = Rc::new(Lambertian::new(albedo));
+                    world.add(Rc::new(Sphere::new(center, 0.2, sphere_material)));
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Vec3::random_from_values(0.5, 1.0);
+                    let fuzz = random_double_from_values(0.0, 0.5);
+                    let sphere_material = Rc::new(Metal::new(albedo, fuzz));
+                    world.add(Rc::new(Sphere::new(center, 0.2, sphere_material)));
+                } else {
+                    // glass
+
+                    let sphere_material = Rc::new(Dielectric::new(1.5));
+                    world.add(Rc::new(Sphere::new(center, 0.2, sphere_material)));
+                }
+            }
+        }
+    }
+
+    let material1 = Rc::new(Dielectric::new(1.5));
+    world.add(Rc::new(Sphere::new(
+        Vec3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
+    )));
+
+    let material2 = Rc::new(Lambertian::new(Vec3::new(0.4, 0.2, 0.1)));
+    world.add(Rc::new(Sphere::new(
+        Vec3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
+    )));
+
+    let material3 = Rc::new(Metal::new(Vec3::new(0.7, 0.6, 0.5), 0.0));
+    world.add(Rc::new(Sphere::new(
+        Vec3::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
+    )));
+
+    world
+}
+
 fn main() {
-    let image_width = 200;
-    let image_height = 100;
+    let image_width = 1200;
+    let image_height = 800;
     let samples_per_pixel = 100;
     let max_depth = 50;
 
     print!("P3\n{} {}\n255\n", image_width, image_height);
 
-    let mut world = HitAbleList::new();
+    let world = random_scene();
 
-    world.add(Rc::new(Sphere::new(
-        Vec3::new(0.0, 0.0, -1.0),
-        0.5,
-        Rc::new(Lambertian::new(Vec3::new(0.1, 0.2, 0.5))),
-    )));
-
-    world.add(Rc::new(Sphere::new(
-        Vec3::new(0.0, -100.5, -1.0),
-        100.0,
-        Rc::new(Lambertian::new(Vec3::new(0.8, 0.8, 0.0))),
-    )));
-
-    world.add(Rc::new(Sphere::new(
-        Vec3::new(1.0, 0.0, -1.0),
-        0.5,
-        Rc::new(Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.0)),
-    )));
-
-    world.add(Rc::new(Sphere::new(
-        Vec3::new(-1.0, 0.0, -1.0),
-        0.5,
-        Rc::new(Dielectric::new(1.5)),
-    )));
-    world.add(Rc::new(Sphere::new(
-        Vec3::new(-1.0, 0.0, -1.0),
-        -0.45,
-        Rc::new(Dielectric::new(1.5)),
-    )));
-
-    let lookfrom = Vec3::new(3.0, 3.0, 2.0);
-    let lookat = Vec3::new(0.0, 0.0, -1.0);
+    let lookfrom = Vec3::new(13.0, 2.0, 3.0);
+    let lookat = Vec3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
 
-    let dist_to_focus = (lookfrom - lookat).length();
-    let aperture = 2.0;
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
 
     let cam = Camera::new(
         lookfrom,
