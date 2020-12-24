92d6fe7e6885a3d932ff8641fefff24d5f040249
Demonus <pogon.ap@hotmail.com>
2020-12-24 20:50:10 +0000

    2.5.24 Using Random Vectors on the Lattice Points

--- src/texture.rs
+++ src/texture.rs
@@ -56,7 +56,7 @@ impl Texture for CheckerTexture {
 }
 
 pub struct PerlinTexture {
-    ranfloat: Vec<f64>,
+    ranvec: Vec<Vec3>,
     perm_x: Vec<i32>,
     perm_y: Vec<i32>,
     perm_z: Vec<i32>,
@@ -65,14 +65,14 @@ pub struct PerlinTexture {
 
 impl PerlinTexture {
     pub fn new(scale: f64) -> Self {
-        let mut ran_temp: Vec<f64> = vec![];
+        let mut ran_temp: Vec<Vec3> = vec![];
 
         for _ in 0..POINT_COUNT {
-            ran_temp.push(random_double());
+            ran_temp.push(Vec3::random_from_values(-1.0, 1.0));
         }
 
         Self {
-            ranfloat: ran_temp,
+            ranvec: ran_temp,
             perm_x: perlin_generate_perm(),
             perm_y: perlin_generate_perm(),
             perm_z: perlin_generate_perm(),
@@ -81,24 +81,20 @@ impl PerlinTexture {
     }
 
     fn noise(&self, p: &Vec3) -> f64 {
-        let mut u = p.x() - p.x().floor();
-        let mut v = p.y() - p.y().floor();
-        let mut w = p.z() - p.z().floor();
-
-        u = u * u * (3.0 - 2.0 * u);
-        v = v * v * (3.0 - 2.0 * v);
-        w = w * w * (3.0 - 2.0 * w);
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
         let i = p.x().floor() as i32;
         let j = p.y().floor() as i32;
         let k = p.z().floor() as i32;
 
-        let mut c = [[[0f64; 2]; 2]; 2];
+        let mut c = [[[Vec3::empty(); 2]; 2]; 2];
 
         for di in 0..2 {
             for dj in 0..2 {
                 for dk in 0..2 {
-                    c[di][dj][dk] = self.ranfloat[(self.perm_x[((i + di as i32) & 255) as usize]
+                    c[di][dj][dk] = self.ranvec[(self.perm_x[((i + di as i32) & 255) as usize]
                         ^ self.perm_y[((j + dj as i32) & 255) as usize]
                         ^ self.perm_z[((k + dk as i32) & 255) as usize])
                         as usize];
@@ -106,13 +102,13 @@ impl PerlinTexture {
             }
         }
 
-        trilinear_interp(&mut c, u, v, w)
+        perlin_interp(&mut c, u, v, w)
     }
 }
 
 impl Texture for PerlinTexture {
     fn value(&self, u: f64, v: f64, p: &Vec3) -> Vec3 {
-        return Vec3::new(1.0, 1.0, 1.0) * self.noise(&(self.scale * *p));
+        return Vec3::new(1.0, 1.0, 1.0) * 0.5 * (1.0 + self.noise(&(self.scale * *p)));
     }
 }
 
@@ -137,19 +133,24 @@ fn permute(p: &mut [i32], n: i32) {
     }
 }
 
-fn trilinear_interp(c: &mut [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+fn perlin_interp(c: &mut [[[Vec3; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    let uu = u * u * (3.0 - 2.0 * u);
+    let vv = v * v * (3.0 - 2.0 * v);
+    let ww = w * w * (3.0 - 2.0 * w);
     let mut accum = 0.0;
 
     for i in 0..2 {
         for j in 0..2 {
             for k in 0..2 {
-                accum += (i as f64 * u + (1.0 - i as f64) * (1.0 - u))
-                    * (j as f64 * v + (1.0 - j as f64) * (1.0 - v))
-                    * (k as f64 * w + (1.0 - k as f64) * (1.0 - w))
-                    * c[i][j][k];
+                let weight_v = Vec3::new(u - i as f64, v - j as f64, w - k as f64);
+
+                accum += (i as f64 * uu + (1.0 - i as f64) * (1.0 - uu))
+                    * (j as f64 * vv + (1.0 - j as f64) * (1.0 - vv))
+                    * (k as f64 * ww + (1.0 - k as f64) * (1.0 - ww))
+                    * Vec3::dot(&c[i][j][k], &weight_v);
             }
         }
     }
 
-    return accum;
+    accum
 }
